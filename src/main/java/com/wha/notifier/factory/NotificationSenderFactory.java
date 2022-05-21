bc2bfd2e7f19f8c92b3786d393efd86048393227
Felix Vadcard <vadcard.felix@gmail.com>
2022-05-21 14:09:16 +0200

    solution 2 builder

--- src/main/java/com/wha/notifier/factory/NotificationSenderFactory.java
+++ src/main/java/com/wha/notifier/factory/NotificationSenderFactory.java
@@ -1,10 +1,7 @@
 package com.wha.notifier.factory;
 
 import com.wha.notifier.NotificationSender;
-import com.wha.notifier.decorator.EmailNotificationDecorator;
-import com.wha.notifier.decorator.PhoneNotificationDecorator;
-import com.wha.notifier.decorator.SocialMediaNotificationDecorator;
-import com.wha.notifier.model.BaseNotification;
+import com.wha.notifier.factory.builder.NotificationSenderBuilder;
 import com.wha.notifier.model.EmailNotification;
 import com.wha.notifier.model.PhoneNotification;
 import com.wha.notifier.model.SocialMediaNotification;
@@ -24,25 +21,25 @@ public class NotificationSenderFactory {
   }
 
   public static NotificationSender newEmailAndPhoneNotification(String username, String email, Integer phoneNumber) {
-    BaseNotification baseNotification = new BaseNotification(username);
-    baseNotification = new EmailNotificationDecorator(baseNotification, email);
-    return new PhoneNotificationDecorator(baseNotification, phoneNumber);
+    return NotificationSenderBuilder.builder(username)
+      .withEmail(email)
+      .withPhone(phoneNumber)
+      .build();
   }
 
   public static NotificationSender newEmailAndSocialMediaNotification(String username,String email, String socialMediaUsername) {
-    BaseNotification baseNotification = new BaseNotification(username);
-    baseNotification = new EmailNotificationDecorator(baseNotification, email);
-    return new SocialMediaNotificationDecorator(baseNotification, socialMediaUsername);
+    return NotificationSenderBuilder.builder(username)
+      .withEmail(email)
+      .withSocialMedia(socialMediaUsername)
+      .build();
   }
 
-  public static NotificationSender newEmailAndSocialMediaAndPhoneNotification(String username,
-                                                                              String email,
-                                                                              String socialMediaUsername,
-                                                                              Integer phoneNumber) {
-    BaseNotification baseNotification = new BaseNotification(username);
-    baseNotification = new EmailNotificationDecorator(baseNotification, email);
-    baseNotification = new SocialMediaNotificationDecorator(baseNotification, socialMediaUsername);
-    return new PhoneNotificationDecorator(baseNotification, phoneNumber);
 
+  public static NotificationSender newEmailAndSocialMediaAndPhoneNotification(String username, String email, String socialMediaUsername, Integer phoneNumber) {
+    return NotificationSenderBuilder.builder(username)
+      .withEmail(email)
+      .withSocialMedia(socialMediaUsername)
+      .withPhone(phoneNumber)
+      .build();
   }
 }
